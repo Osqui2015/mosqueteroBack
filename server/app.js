fd5a54436fa19d2be25486f58628bc0417926d7b
Mariabernardita <mariabernardita.15@gmail.com>
2022-12-04 20:02:46 -0300

    Authentication

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import postsRoutes from './routes/posts.routes.js';
+import authRoutes from './routes/auth.routes.js';
 import morgan from "morgan";
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.urlencoded({extended: false }));
 
 
 app.use(postsRoutes);
+app.use(authRoutes);
 
 export default  app ;
 
