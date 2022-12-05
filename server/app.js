6b68bd62e8dc316110934345d60e1e9f3f12ad30
Osqui2015 <68171383+Osqui2015@users.noreply.github.com>
2022-12-05 17:10:42 -0300

    add cors
    add cors

--- server/app.js
+++ server/app.js
@@ -2,13 +2,14 @@ import express from "express"
 import postsRoutes from './routes/posts.routes.js';
 import authRoutes from './routes/auth.routes.js';
 import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false }));
-
+app.use(cors());
 
 app.use(postsRoutes);
 app.use(authRoutes);
